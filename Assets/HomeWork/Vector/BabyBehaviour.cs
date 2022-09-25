83d925788c475b4ba5df5031ec932e432e2dbb8b
georgegabor <georgegabor@rocketmail.com>
2022-09-25 18:26:10 +0200

    Add behaviour

--- Assets/HomeWork/Vector/BabyBehaviour.cs
+++ Assets/HomeWork/Vector/BabyBehaviour.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BabyBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 10;
+
+    [SerializeField]
+    float angularSpeed = 180;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,5 +18,43 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get directions
+        Vector3 velocity = KeyboardInputsHandler();
+
+        // Objects x must be -90 to stand straight up
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(-90, eulerRotation.y, 0);
+
+        transform.position += velocity * Time.deltaTime * speed;
+
+        var step = angularSpeed * Time.deltaTime;
+
+        if (velocity != Vector3.zero)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(velocity);
+
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, targetRot, step);
+        }
+    }
+
+    Vector3 KeyboardInputsHandler()
+    {
+        bool up = Input.GetKey(KeyCode.UpArrow);
+        bool down = Input.GetKey(KeyCode.DownArrow);
+        bool left = Input.GetKey(KeyCode.LeftArrow);
+        bool right = Input.GetKey(KeyCode.RightArrow);
+
+        float x = 0;
+        float y = 0;
+
+        if (right) x += 1;
+        if (left) x += -1;
+        if (up) y += 1;
+        if (down) y += -1;
+
+        Vector3 velocity = new Vector3(x, 0, y);
+        velocity.Normalize();
+        return velocity;
     }
 }
---
+++ Assets/HomeWork/Vector/ShibaBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShibaBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Cartoon Baby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float maxStep = speed * Time.deltaTime;
+        Vector3 targetPoint = target.transform.position;
+        Vector3 selfPoint = transform.position;
+
+        // Objects x must be -90 to stand straight up
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(-90, eulerRotation.y, 90);
+
+        // transform.position =
+        //     Vector3.MoveTowards(selfPoint, targetPoint, maxStep);
+        if ((targetPoint - selfPoint) != Vector3.zero)
+        {
+            // transform.rotation =
+            //     Quaternion.LookRotation(targetPoint - selfPoint);
+            Quaternion targetRot =
+                Quaternion.LookRotation(targetPoint - selfPoint);
+
+            transform.rotation =
+                Quaternion
+                    .RotateTowards(transform.rotation, targetRot, maxStep);
+        }
+    }
+}
