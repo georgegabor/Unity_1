cbd69a80f7df8fd9f17ee99c95cf9c3f63070fec
George Gabor <tesztahal@gmail.com>
2022-10-04 19:21:18 +0200

    Add text on canvas

--- Assets/FirstGame/Scripts/Collectable.cs
+++ Assets/FirstGame/Scripts/Collectable.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Collectable : MonoBehaviour
 {
-    [SerializeField] int sum;
+    [SerializeField] int sum = 0;
+    [SerializeField] TMP_Text displayedText;
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateText();
     }
 
     // Update is called once per frame
@@ -20,6 +23,12 @@ public class Collectable : MonoBehaviour
     public void Collect(int value)
     {
         sum += value;
-        Debug.Log("sum: " + sum);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (displayedText != null)
+            displayedText.text = "Points: " + sum;
     }
 }
