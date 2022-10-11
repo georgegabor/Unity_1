ef85959f8eea5a12bbede4fabe6f27256a5c69ac
George Gabor <tesztahal@gmail.com>
2022-10-11 20:24:04 +0200

    Add horizontal move

--- Assets/PhysicsTest/CharacterMover2D.cs
+++ Assets/PhysicsTest/CharacterMover2D.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class CharacterMover2D : MonoBehaviour
 {
+    [Header("Inputs")]
     [SerializeField] new Rigidbody2D rigidbody;
     [SerializeField] float jumpForce = 5;
+    [SerializeField] float moveForce = 5;
     [SerializeField] int airJumps = 3;
 
     bool isOnGround = false;
@@ -20,11 +22,20 @@ public class CharacterMover2D : MonoBehaviour
 
         if (isJump && (isOnGround || jumpCounts > 0))
         {
-            rigidbody.velocity = Vector2.zero;
+            Vector2 v = rigidbody.velocity;
+            v.y = 0;
+            rigidbody.velocity = v;
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse
                 );
             jumpCounts--;
         }
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        rigidbody.AddForce(Vector2.right * x * moveForce);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
