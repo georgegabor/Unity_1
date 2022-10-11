b079a76bdb75e79fadfb5927be9319a7fa9e9f9f
George Gabor <tesztahal@gmail.com>
2022-10-11 19:42:28 +0200

    Add airjump

--- Assets/PhysicsTest/CharacterMover2D.cs
+++ Assets/PhysicsTest/CharacterMover2D.cs
@@ -4,8 +4,10 @@ public class CharacterMover2D : MonoBehaviour
 {
     [SerializeField] new Rigidbody2D rigidbody;
     [SerializeField] float jumpForce = 5;
+    [SerializeField] int airJumps = 3;
 
     bool isOnGround = false;
+    int jumpCounts = 0;
 
     private void OnValidate()
     {
@@ -16,17 +18,19 @@ public class CharacterMover2D : MonoBehaviour
     {
         bool isJump = Input.GetKeyDown(KeyCode.Space);
 
-        if (isJump && isOnGround)
+        if (isJump && (isOnGround || jumpCounts > 0))
         {
             rigidbody.velocity = Vector2.zero;
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse
                 );
+            jumpCounts--;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isOnGround = true;
+        jumpCounts = airJumps + 1;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
