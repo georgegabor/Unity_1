267a97e3b9e87d5e28c6b86f5601f3f56f1c8246
George Gabor <tesztahal@gmail.com>
2022-10-18 19:48:13 +0200

    Add Explosion with Unity method

--- Assets/PhysicsTest/ClickExplosion.cs
+++ Assets/PhysicsTest/ClickExplosion.cs
@@ -5,6 +5,7 @@ public class ClickExplosion : MonoBehaviour
     [Header("Inputs")]
     [SerializeField] float _maxForce = 5;
     [SerializeField] float _range = 10;
+    [SerializeField] float _upward = 3;
 
     Rigidbody[] _rigidbodies;
 
@@ -28,28 +29,36 @@ public class ClickExplosion : MonoBehaviour
             {
                 _lastRayHit = _hit.point;
                 // Debug.Log("Name: " + _hit.collider.name + " Point:  " + _lastRayHit);
-                _Explode(_lastRayHit);
+                _ExplodeAll(_lastRayHit);
             }
         }
     }
 
-    void _Explode(Vector3 position)
+    void _ExplodeAll(Vector3 position)
     {
         for (int i = 0; i < _rigidbodies.Length; i++)
         {
             Rigidbody _rigidbody = _rigidbodies[i];
+            _rigidbody.AddExplosionForce(_maxForce, position, _range, _upward, ForceMode.Impulse);
 
-            Vector3 distanceVect = _rigidbody.position - position;
-            float distance = distanceVect.magnitude;
-            Vector3 direction = distanceVect.normalized;
 
-            if (distance < _range)
-            {
-                float force = _maxForce * (_range - distance) / _range;
 
-                _rigidbody.AddForce(direction * force, ForceMode.Impulse);
+            // _EplodeOne(position, _rigidbody);
 
-            }
+        }
+    }
+
+    private void _EplodeOne(Vector3 position, Rigidbody _rigidbody)
+    {
+        Vector3 distanceVect = _rigidbody.position - position;
+        float distance = distanceVect.magnitude;
+        Vector3 direction = distanceVect.normalized;
+
+        if (distance < _range)
+        {
+            float force = _maxForce * (_range - distance) / _range;
+
+            _rigidbody.AddForce(direction * force, ForceMode.Impulse);
 
         }
     }
