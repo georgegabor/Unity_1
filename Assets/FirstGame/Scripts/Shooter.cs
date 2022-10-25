0e15db13392cbd74422650735ae797ea0a87727e
George Gabor <tesztahal@gmail.com>
2022-10-25 19:46:16 +0200

    Add bullet

---
+++ Assets/FirstGame/Scripts/Shooter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [Header("Inputs")]
+    [SerializeField] GameObject projectile;
+    [SerializeField] float speed = 25;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            projectile.transform.position = transform.position;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            Vector3 direction = transform.forward;
+
+            Vector3 v = transform.TransformVector(Vector3.up);
+            Vector3 v2 = transform.InverseTransformVector(v);
+            direction.Normalize();
+
+            rb.velocity = direction * speed;
+        }
+    }
+}
