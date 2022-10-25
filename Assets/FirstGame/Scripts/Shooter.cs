0b9d7dbc592dcb1160d4594476e024a3b54f3955
George Gabor <tesztahal@gmail.com>
2022-10-25 20:56:52 +0200

    Add enum

--- Assets/FirstGame/Scripts/Shooter.cs
+++ Assets/FirstGame/Scripts/Shooter.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    enum ShootingPattern
+    {
+        Random, Sequence
+    }
+
     [Header("Inputs")]
     [SerializeField] GameObject[] projectilePrototypes;
     [SerializeField] float speed = 25;
+    [SerializeField] ShootingPattern pattern = ShootingPattern.Random;
+
+    int count = 0;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            int randomNum = Random.Range(0, projectilePrototypes.Length);
-            GameObject prot = projectilePrototypes[randomNum];
+            GameObject proto;
+            int randomNum = 0;
 
-            GameObject projectile = Instantiate(prot);
+            if (pattern == ShootingPattern.Random)
+            {
+                randomNum = Random.Range(0, projectilePrototypes.Length);
+                proto = projectilePrototypes[randomNum];
+
+            }
+            else
+            {
+                int index = count % projectilePrototypes.Length;
+                proto = projectilePrototypes[index]; ;
+            }
+
+
+            GameObject projectile = Instantiate(proto);
             projectile.transform.position = transform.position;
 
             Rigidbody rb = projectile.GetComponent<Rigidbody>();
@@ -27,6 +48,8 @@ public class Shooter : MonoBehaviour
             direction.Normalize();
 
             rb.velocity = direction * speed;
+
+            count++;
         }
     }
 }
