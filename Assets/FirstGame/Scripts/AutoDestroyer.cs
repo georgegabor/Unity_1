0769eb870c89c44ba12ca8fe5f67d114424c7618
George Gabor <tesztahal@gmail.com>
2022-10-25 20:20:02 +0200

    Add auto destroyer

--- Assets/FirstGame/Scripts/AutoDestroyer.cs
+++ Assets/FirstGame/Scripts/AutoDestroyer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class AutoDestroyer : MonoBehaviour
 {
+
+    [Header("Inputs")]
+    [SerializeField] float destructionTime = 1;
+
+    float startTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float currentTime = Time.time;
+
+        if (currentTime - startTime > destructionTime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
